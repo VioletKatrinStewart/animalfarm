2d675322783616e656182ae96e22b1799405ffb2
Violet Stewart <vkazoo2@gmail.com>
2021-08-25 16:51:55 -0700

    changed click to pet

--- app.js
+++ app.js
@@ -1,28 +1,41 @@
 // import functions
 const catImage=document.getElementById('cat-image');
 const catAudio=document.getElementById('cat');
-
+const catSpan=document.getElementById('cat-clicks');
+const dogSpan=document.getElementById('dog-clicks');
+const horseSpan=document.getElementById('horse-clicks');
 const dogImage=document.getElementById('dog-image');
 const dogAudio=document.getElementById('dog');
 
 const horseImage=document.getElementById('horse-image');
 const horseAudio=document.getElementById('horse');
 
+let catClicks = 0; 
+let dogClicks = 0;
+let horseClicks = 0;
+
 catImage.addEventListener('click', () => {
   
     catAudio.play();
-
+    catClicks ++
+    catSpan.textContent = 'You pet the cat ' + catClicks + ' times';
 });
 
 dogImage.addEventListener('click', () => {
 
     dogAudio.play();
+    dogClicks ++ 
+    dogSpan.textContent = 'You pet the dog ' + dogClicks + ' times';
 });
 
 horseImage.addEventListener('click', () => {
     
     horseAudio.play();
+    horseClicks ++ 
+    horseSpan.textContent = 'You pet the horse ' + horseClicks + ' times';
 });
+
+
 
 
 
