f703da79b85cd85a7ee7948342ea4a0835e9a39f
Violet Stewart <vkazoo2@gmail.com>
2021-08-25 16:06:52 -0700

    added animal pictures and sounds

--- app.js
+++ app.js
@@ -1,4 +1,30 @@
 // import functions
+const catImage=document.getElementById('cat-image');
+const catAudio=document.getElementById('cat');
+
+const dogImage=document.getElementById('dog-image');
+const dogAudio=document.getElementById('dog');
+
+const horseImage=document.getElementById('horse-image');
+const horseAudio=document.getElementById('horse');
+
+catImage.addEventListener('click', () => {
+  
+    catAudio.play();
+
+});
+
+dogImage.addEventListener('click', () => {
+
+    dogAudio.play();
+});
+
+horseImage.addEventListener('click', () => {
+    
+    horseAudio.play();
+});
+
+
 
 // reference needed DOM elements
 
